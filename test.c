eadac22cbaa05a8bc27b9dbf1cb32c0ed078ac57
Andrew-liu <1095511864@qq.com>
2014-06-20 18:51:37 -0700

    first

---
+++ test.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: andrew_liu
+	> Mail:1095511864@qq.com 
+	> Created Time: Fri 20 Jun 2014 06:44:19 PM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+int main(int argc,char *argv)
+{
+	return 0;
+}
---
+++ test1.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: test1.c
+	> Author: andrew_liu
+	> Mail:1095511864@qq.com 
+	> Created Time: Fri 20 Jun 2014 06:50:10 PM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+int main(int argc,char *argv)
+{
+	int a;
+	return 0;
+}
